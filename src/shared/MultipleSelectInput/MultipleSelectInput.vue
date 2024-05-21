bbc6c92c8fdb4b2e898547ec34058040b27bca46
Anna Denisova <abachledacurus@gmail.com>
2024-05-21 13:31:44 +0700

    feat: add values render

--- src/shared/MultipleSelectInput/MultipleSelectInput.vue
+++ src/shared/MultipleSelectInput/MultipleSelectInput.vue
@@ -2,12 +2,23 @@
 import { ref } from "vue";
 import Modal from "../Modal/Modal.vue";
 import editIcon from "/edit.svg";
+import closeIcon from "/close.svg";
 
 const modalOpen = ref<boolean>(false);
+const values = ref<string[]>(["1", "2", "item"]);
+function deleteValueHandler(value: string) {
+  values.value = values.value.filter((item) => item !== value);
+}
 </script>
 
 <template>
   <div class="input">
+    <ul class="values">
+      <li v-for="value in values" :key="value" class="values__value">
+        {{ value }}
+        <img :src="closeIcon" alt="delete" @click="deleteValueHandler(value)" class="delete-icon"/>
+      </li>
+    </ul>
     <img :src="editIcon" alt="edit" @click="modalOpen = true"/>
     <Modal :modalOpen="modalOpen" @close="modalOpen = false">
     </Modal>
@@ -26,5 +37,19 @@ const modalOpen = ref<boolean>(false);
     width: 24px;
     height: 24px;
     cursor: pointer;
+  }
+  .values {
+    display: flex;
+    padding: 0;
+    margin: 0;
+  }
+  .values__value {
+    list-style-type: none;
+    background-color: #F2F2F2;
+    border-radius: 8px;
+    padding: 8px;
+    display: flex;
+    align-items: center;
+    margin-right: 8px;
   }
 </style>
