d0adec147ebfd742cf45f7d7f5773c9d12f3655e
Anna Denisova <abachledacurus@gmail.com>
2024-05-21 13:29:10 +0700

    feat: add modal

---
+++ src/shared/Modal/Modal.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import closeIcon from "/close.svg";
+
+defineProps<{ modalOpen: boolean }>();
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+  <teleport to="body">
+    <div v-if="modalOpen" class="modal" @click="emit('close')">
+      <div class="modal__content" @click.stop>
+        <img :src="closeIcon" alt="close" @click="emit('close')" class="modal__close"/>
+        <slot name="modalInput"></slot>
+        <slot name="options"></slot>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<style scoped>
+  .modal {
+    position: absolute;
+    top: 0; 
+    right: 0; 
+    bottom: 0; 
+    left: 0;
+    background-color: rgba(0 ,0, 0, 0.5);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .modal__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #FFFFFF;
+    width: 300px;
+    height: 300px;
+    padding: 20px;
+    border-radius: 20px;
+  }
+  .modal__close {
+    margin-left: auto;
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+  }
+</style>
---
+++ src/shared/MultipleSelectInput/MultipleSelectInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Modal from "../Modal/Modal.vue";
+import editIcon from "/edit.svg";
+
+const modalOpen = ref<boolean>(false);
+</script>
+
+<template>
+  <div class="input">
+    <img :src="editIcon" alt="edit" @click="modalOpen = true"/>
+    <Modal :modalOpen="modalOpen" @close="modalOpen = false">
+    </Modal>
+  </div>
+</template>
+
+<style scoped>
+  .input {
+    display: flex;
+    align-items: center;
+    border-radius: 20px;
+    padding: 12px;
+    border: 1px solid #F2F2F2;
+  }
+  img {
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+  }
+</style>
